baf7608b7f1f0a809aa6629c29587f9d6a8f0ea0
JidnyasaBhagat0525 <jidnyasabhagat7@gmail.com>
2026-06-28 19:17:53 +0530

    Ass user choice menu

---
+++ student_management.py
@@ -0,0 +1,19 @@
+choice = int(input("Enter your choice: "))
+
+if choice == 1:
+  print("Add Student Selected")
+
+elif choice == 2:
+    print("Display Student Selected")
+
+elif choice == 3:
+    print("Search Student Selected")
+
+elif choice == 4:
+    print("Delete Student Selected")
+
+elif choice == 5:
+    print("Thank You")
+
+else:
+  print("Invalid Choice")
